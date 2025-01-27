d29714442a9d5c058052ae14e2a10b23292cbd30
Goooble <[bivinbheemaiah@gmail.com]>
2025-01-27 22:57:57 +0530

    Feat: Code ship object

--- src/ship.js
+++ src/ship.js
@@ -1,5 +1,27 @@
-let ship = () => {
-  return {};
+let ship = (length) => {
+  let hitsTaken = 0;
+  let state = true;
+  function hit() {
+    hitsTaken++;
+  }
+  function isSunk() {
+    if (hitsTaken >= length) state = false;
+  }
+  return {
+    get length() {
+      return length;
+    },
+    get hitsTaken() {
+      return hitsTaken;
+    },
+    get state() {
+      return state;
+    },
+    hit,
+    isSunk,
+  };
+  //do we really need state and isSunk both? isnt isSunk enough
+  //making state redundant?
 };
 
 export default ship;
--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,7 +1,29 @@
 import ship from "../ship.js";
 
 describe("ship tester", () => {
-  test("returns object", () => {
-    expect(ship()).toEqual({});
+  let testShip = ship(3);
+  test("length", () => {
+    expect(testShip.length).toBe(3);
+  });
+  test("number of hits", () => {
+    expect(testShip.hitsTaken).toBe(0);
+  });
+  test("state", () => {
+    expect(testShip.state).toBe(true);
+  });
+  test("hit function", () => {
+    let hits = testShip.hitsTaken;
+    testShip.hit();
+    expect(testShip.hitsTaken).toBe(hits + 1);
+  });
+  test("isSunk true test", () => {
+    testShip.isSunk();
+    expect(testShip.state).toBe(true);
+  });
+  test("isSunk false test", () => {
+    testShip.hit();
+    testShip.hit();
+    testShip.isSunk(); //initially true now changes to status to false
+    expect(testShip.state).toBe(false);
   });
 });
